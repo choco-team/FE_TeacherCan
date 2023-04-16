e8a65b24b3d1153a87f378400fe233eeaede2039
박겸영 <yong_drg@naver.com>
2023-04-17 05:40:54 +0900

    feat: add common style

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,13 @@
 import * as S from './style';
 
 function Landing() {
-  return <S.Title>Landing</S.Title>;
+  return (
+    <S.Container>
+      <S.Title>Landing</S.Title>
+      <S.Button>작은 버튼</S.Button>
+      <S.BigButton>큰 버튼</S.BigButton>
+    </S.Container>
+  );
 }
 
 export default Landing;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,20 @@
-const theme = {};
+const theme = {
+  color: {
+    white: '#ffffff',
+    gray: {
+      50: '#fafafa',
+      100: '#f4f4f5',
+      200: '#e4e4e7',
+      300: '#d4d4d8',
+      400: '#a1a1aa',
+      500: '#71717a',
+      600: '#52525b',
+      700: '#3f3f46',
+      800: '#27272a',
+      900: '#18181b',
+      950: '#09090b',
+    },
+  },
+};
 
 export default theme;
