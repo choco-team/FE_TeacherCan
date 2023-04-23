84f484565b732e923f8a307dc388af8cda936f13
soave424 <soave424@gmail.com>
2023-04-23 21:53:03 +0900

    feat : joe issue

--- src/constant/routePath.ts
+++ src/constant/routePath.ts
@@ -1,6 +1,7 @@
 const ROUTE_PATH = {
   landing: '/',
   hd: '/hd',
+  joe: '/joe',
 } as const;
 
 export default ROUTE_PATH;
---
+++ src/pages/JOE/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './style';
+
+const Joe = () => {
+  return <S.Button>JOE TEST</S.Button>;
+};
+
+export default Joe;
