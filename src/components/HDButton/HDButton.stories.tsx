bdb45d4e2148e2144b771f9acb092dd3c4dadbe4
nlom0218 <nlom0218@naver.com>
2023-05-07 21:39:06 +0900

    test: HDButton 스토리 생성

---
+++ src/components/HDButton/HDButton.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+/**
+ * `HDButton` 컴포넌트는 연습 버튼 컴포넌트입니다.
+ */
+const meta: Meta<typeof HDButton> = {
+  title: 'HDButton',
+  component: HDButton,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof HDButton>;
+
+export const HDButtonDefault: Story = {};
