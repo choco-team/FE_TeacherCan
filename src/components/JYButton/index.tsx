1eff9523da4acea3366b3faa4579fc245dcfd335
jangjiyoon <jjswan2011@gmail.com>
2023-05-07 22:01:13 +0900

    feat: JYButton 색깔 및 텍스트 지

--- src/components/JYButton/index.tsx
+++ src/components/JYButton/index.tsx
@@ -1,5 +1,20 @@
-function JYButton() {
-  return <button></button>;
+import styled from 'styled-components';
+
+type ButtonType = {
+  backgroundColor: string;
+};
+
+const Button = styled.button<ButtonType>`
+  background-color: ${(props) => props.backgroundColor};
+`;
+
+type JYButtonProps = {
+  text: string;
+  backgroundColor: string;
+};
+
+function JYButton({ text, backgroundColor }: JYButtonProps) {
+  return <Button backgroundColor={backgroundColor}>{text}</Button>;
 }
 
 export default JYButton;
