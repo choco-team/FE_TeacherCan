6fbbc55b61b2107713905ce778f55d128de8c888
nlom0218 <nlom0218@naver.com>
2023-05-07 21:52:33 +0900

    feat: HDButton 컴포넌트에 text, backgroundColor props 전달

--- src/components/HDButton/index.tsx
+++ src/components/HDButton/index.tsx
@@ -1,5 +1,25 @@
-function HDButton() {
-  return <></>;
+import styled from 'styled-components';
+
+import * as GlobalStyle from '@Styles/common';
+
+type ButtonType = {
+  backgroundColor: string;
+};
+
+const Button = styled(GlobalStyle.Button)<ButtonType>`
+  background-color: ${(props) => props.backgroundColor};
+  :active {
+    background-color: pink;
+  }
+`;
+
+type HDButtonProps = {
+  text: string;
+  backgroundColor: string;
+};
+
+function HDButton({ text, backgroundColor }: HDButtonProps) {
+  return <Button backgroundColor={backgroundColor}>{text}</Button>;
 }
 
 export default HDButton;
