efd54964da3ca249647b4c7e9d36a702e0cef377
nlom0218 <nlom0218@naver.com>
2023-05-28 21:34:20 +0900

    refactor: SummaryList 스토리북 폴더 생성

--- src/components/SummaryList/SummaryList.stories.tsx
+++ src/components/SummaryList/SummaryList.stories.tsx
@@ -8,7 +8,7 @@ type Story = StoryObj<typeof SummaryList>;
  * `SummaryList`은 어떤 주제에 대한 정보를 간단히 나타내고 싶을 때 사용되는 컴포넌트입니다.
  */
 const meta: Meta<typeof SummaryList> = {
-  title: 'SummaryList',
+  title: 'List/SummaryList',
   component: SummaryList,
 };
 
