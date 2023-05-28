31520d66c62693baed2e428cbc3d3a8e9ce4a6f4
nlom0218 <nlom0218@naver.com>
2023-05-28 21:35:13 +0900

    refactor: WeatherBadge 컴포넌트 분리
    상태에 따라 컴포넌트 랜더링
    Badge 스토리북 폴더 생성

--- src/components/WeatherBadge/WeatherBadge.stories.tsx
+++ src/components/WeatherBadge/WeatherBadge.stories.tsx
@@ -10,7 +10,7 @@ type Story = StoryObj<typeof WeatherBadge>;
  * `WeatherBadge`은 현재 날씨, 미세먼지를 간략하게 나타내는 컴포넌트입니다.
  */
 const meta: Meta<typeof WeatherBadge> = {
-  title: 'WeatherBadge',
+  title: 'Badge/WeatherBadge',
   component: WeatherBadge,
 };
 
@@ -39,8 +39,15 @@ export const RejectWeatherBadge: Story = {
 };
 
 /**
+ * 날씨 정보가 없을 때의 `WeatherBadge`의 스토리입니다.
+ */
+export const NoneWeatherBadge: Story = {
+  args: {},
+};
+
+/**
  * 날씨 정보가 불러오는 동안 보여지는 `WeatherBadge`의 스토리입니다.
  */
 export const SkeletonWeatherBadge: Story = {
-  args: {},
+  args: { isLoading: true },
 };
