170adeaf3b4df030833660b4f106427e9238c0ca
nlom0218 <nlom0218@naver.com>
2023-06-10 23:56:42 +0900

    feat: 선택된 Header의 navigationLink 컴포넌트 마크업

--- src/components/NavigationLink/style.ts
+++ src/components/NavigationLink/style.ts
@@ -5,6 +5,15 @@ import { NavigationLink } from './type';
 type LayoutProps = Pick<NavigationLink, 'category' | 'isSelected'>;
 
 export const Layout = styled.div<LayoutProps>`
+  max-width: 12rem;
+
+  padding-bottom: ${(props) => (props.category === 'main' ? '1rem' : '0rem')};
+  border-bottom: ${(props) =>
+    props.category === 'main' && props.isSelected
+      ? `2px solid ${props.theme.accentText}`
+      : 'none'};
+
+  text-align: center;
   font-size: 2rem;
   color: ${(props) =>
     props.isSelected ? props.theme.accentText : props.theme.subText};
