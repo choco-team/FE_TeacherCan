23159e188f5f6f67aea38ca60ebfb3781edf7ba8
nlom0218 <nlom0218@naver.com>
2023-06-10 18:42:51 +0900

    feat: 홈 router 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,10 +4,20 @@ import ROUTE_PATH from '@Constant/routePath';
 
 import Auth from '@Pages/Auth';
 import SignIn from '@Pages/Auth/SignIn';
+import Home from '@Pages/Home';
+
+import App from './App';
 
 const router = createBrowserRouter([
   {
     path: ROUTE_PATH.main,
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
   {
     path: ROUTE_PATH.auth,
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import HomeMemo from './HomeMemo';
+
+function Home() {
+  return (
+    <>
+      <HomeMemo />
+    </>
+  );
+}
+
+export default Home;
