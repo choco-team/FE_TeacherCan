27deec972257b58de223a28517489dc8974bfd23
nlom0218 <nlom0218@naver.com>
2023-06-11 01:39:32 +0900

    feat: Header 컴포넌트 적용

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import { useLocation } from 'react-router-dom';
+
+import { CATEGORIES } from '@Constant/routePath';
+
+import NavigationLink from '@Components/NavigationLink';
+
+import * as S from './style';
+
+function Header() {
+  const { pathname } = useLocation();
+
+  const mainCategories = CATEGORIES.filter(
+    (category) => category.category === 'main',
+  );
+
+  return (
+    <S.Layout>
+      <S.LinkList>
+        {mainCategories.map((category) => (
+          <NavigationLink
+            key={category.name}
+            name={category.name}
+            category={category.category}
+            path={category.path}
+            isSelected={pathname.split('/').includes(category.path)}
+          />
+        ))}
+      </S.LinkList>
+    </S.Layout>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  position: fixed;
+  min-width: 100vw;
+
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+
+  padding: 2rem 2rem 0rem;
+
+  background-color: ${(props) => props.theme.background.basic};
+`;
+
+export const LinkList = styled.ul`
+  grid-column: 3/-3;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+`;
