6add678e8014dfdab492b9662931733669d9b830
nlom0218 <nlom0218@naver.com>
2023-06-10 23:56:17 +0900

    test: SelectedHeaderNavigationLink 스토리 작성

--- src/components/NavigationLink/NavigationLink.stories.tsx
+++ src/components/NavigationLink/NavigationLink.stories.tsx
@@ -16,10 +16,25 @@ const meta: Meta<typeof NavigationLink> = {
 
 export default meta;
 
+/**
+ * `NavigationLink`의 기본 스토리입니다.
+ */
 export const DefaultNavigationLink: Story = {
   args: {
     category: 'main',
     isSelected: false,
+    name: CATEGORIES.curriculum.name,
+    path: CATEGORIES.curriculum.path,
+  },
+};
+
+/**
+ * 선택된 Header의 `NavigationLink` 스토리입니다.
+ */
+export const SelectedHeaderNavigationLink: Story = {
+  args: {
+    category: 'main',
+    isSelected: true,
     name: CATEGORIES.curriculum.name,
     path: CATEGORIES.curriculum.path,
   },
