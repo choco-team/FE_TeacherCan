8b2b916730c4646acdbf6f08494d896e69d94a41
seoltang <yong_drg@naver.com>
2023-06-12 01:21:51 +0900

    feat: 프리텐다드 웹 폰트 적용

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
   }
   
   html {
+    font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
     font-size: 62.5%;
   }
 
