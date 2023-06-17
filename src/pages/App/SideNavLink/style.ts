367ee779d7fd4ad3cfe1c69df03ef4cd53a2cea0
nlom0218 <nlom0218@naver.com>
2023-06-17 21:06:35 +0900

    refactor: SideNavLink flex 속성 변경

--- src/pages/App/SideNavLink/style.ts
+++ src/pages/App/SideNavLink/style.ts
@@ -5,7 +5,7 @@ import { flexCustom } from '@Styles/common';
 export const Layout = styled.div`
   grid-column: 1/3;
 
-  ${flexCustom('column', 'center', 'flex-start')}
+  ${flexCustom('column', 'stretch', 'flex-start')}
 
   padding: 10rem 3rem 3rem;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 4px 0px;
