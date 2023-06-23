efd3028f1c865a019aaf62ac4a534a5ae5d03173
jangjiyoon <jjswan2011@gmail.com>
2023-06-23 19:10:52 +0900

    refactor: VacationDate 컴포넌트 수정

--- src/pages/Home/VacationDate/style.ts
+++ src/pages/Home/VacationDate/style.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
+const ButtonOrange = '#ff8a00';
+
 export const VacationDate = styled.span`
   border-radius: 20px;
-  background-color: #ff8a00;
+  background-color: ${ButtonOrange};
   color: ${(props) => props.theme.text};
   font-weight: 600;
   padding: 5px 30px;
