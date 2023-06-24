f06ba32e90b8b68886c8d9d609d1b2d9d110735d
nlom0218 <nlom0218@naver.com>
2023-06-24 12:00:22 +0900

    refactor: type, style 분리

--- src/pages/App/HeaderBadge/index.tsx
+++ src/pages/App/HeaderBadge/index.tsx
@@ -1,88 +1,34 @@
 import { HiOutlineBell, HiUserCircle } from 'react-icons/hi';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 
 import route from '@Utils/route';
 
-import { flexCustom } from '@Styles/common';
+import * as S from './style';
+import type { HeaderBadge } from './type';
 
-type HeaderBadgeType = {
-  username: string;
-  alarm: boolean;
-};
-
-function HeaderBadge({ username, alarm }: HeaderBadgeType) {
+function HeaderBadge({ username, alarm }: HeaderBadge) {
   return (
-    <Layout>
-      <AlarmContainer>
+    <S.Layout>
+      <S.AlarmContainer>
         <HiOutlineBell />
-        <AlarmDot>{alarm && <Dot></Dot>}</AlarmDot>
-      </AlarmContainer>
-      <HeaderUserContainer>
+        <S.AlarmDot>{alarm && <S.Dot></S.Dot>}</S.AlarmDot>
+      </S.AlarmContainer>
+      <S.HeaderUserContainer>
         <HiUserCircle />
         {username ? (
-          <UserNameWrapper>
-            <UserName>{username}</UserName>
+          <S.UserNameWrapper>
+            <S.UserName>{username}</S.UserName>
             <MdKeyboardArrowDown />
-          </UserNameWrapper>
+          </S.UserNameWrapper>
         ) : (
-          <UserName>
+          <S.UserName>
             <Link to={route.calculatePath(['auth', 'signin'])}>Sign In</Link>
-          </UserName>
+          </S.UserName>
         )}
-      </HeaderUserContainer>
-    </Layout>
+      </S.HeaderUserContainer>
+    </S.Layout>
   );
 }
 
 export default HeaderBadge;
-
-const Layout = styled.div`
-  ${flexCustom('row', 'center', 'normal')}
-  svg {
-    ${flexCustom('row', 'center', 'normal')}
-    font-size: 2.5rem;
-  }
-`;
-
-const AlarmContainer = styled.div`
-  position: relative;
-
-  margin-right: 10px;
-
-  cursor: pointer;
-`;
-
-const AlarmDot = styled.div`
-  position: absolute;
-  bottom: 0;
-  right: -2px;
-`;
-
-const Dot = styled.div`
-  width: 10px;
-  height: 10px;
-  border-radius: 50%;
-
-  background-color: ${(props) => props.theme.accentText};
-`;
-
-const HeaderUserContainer = styled.div`
-  ${flexCustom('row', 'center', 'normal')}
-  background-color: ${(props) => props.theme.background.primary};
-  color: ${(props) => props.theme.background.basic};
-
-  padding: 5px 10px;
-  border-radius: 20px;
-
-  cursor: pointer;
-`;
-
-const UserNameWrapper = styled.div`
-  ${flexCustom('row', 'center', 'normal')}
-`;
-
-const UserName = styled.div`
-  margin: 0px 5px;
-`;
---
+++ src/pages/App/HeaderBadge/style.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+import { flexCustom } from '@Styles/common';
+
+export const Layout = styled.div`
+  ${flexCustom('row', 'center', 'normal')}
+  svg {
+    ${flexCustom('row', 'center', 'normal')}
+    font-size: 2.5rem;
+  }
+`;
+
+export const AlarmContainer = styled.div`
+  position: relative;
+
+  margin-right: 10px;
+
+  cursor: pointer;
+`;
+
+export const AlarmDot = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: -2px;
+`;
+
+export const Dot = styled.div`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+
+  background-color: ${(props) => props.theme.accentText};
+`;
+
+export const HeaderUserContainer = styled.div`
+  ${flexCustom('row', 'center', 'normal')}
+  background-color: ${(props) => props.theme.background.primary};
+  color: ${(props) => props.theme.background.basic};
+
+  padding: 5px 10px;
+  border-radius: 20px;
+
+  cursor: pointer;
+`;
+
+export const UserNameWrapper = styled.div`
+  ${flexCustom('row', 'center', 'normal')}
+`;
+
+export const UserName = styled.div`
+  margin: 0px 5px;
+`;
