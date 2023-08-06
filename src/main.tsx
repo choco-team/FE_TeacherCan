59ea3e7801a8a09b38d4cabc74bdfba249b1e1f1
nlom0218 <nlom0218@naver.com>
2023-08-06 19:37:03 +0900

    fix: worker 실행 환경 수정

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import { RouterProvider } from 'react-router-dom';
 import router from './Router';
 import { worker } from './mocks/worker';
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start();
-}
+// if (process.env.NODE_ENV === 'development') {
+// }
+worker.start();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
