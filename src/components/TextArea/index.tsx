298697b0b67e68b9dd846c01adc73dc1f9a3dbef
박겸영 <yong_drg@naver.com>
2023-10-02 17:45:48 +0900

    feat: TextArea 스타일 수정 및 props type 수정

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -1,8 +1,13 @@
 import * as S from './style';
 import * as T from './type';
 
-function TextArea({ placeholder, border = 'solid', rows = 5 }: T.TextArea) {
-  return <S.TextArea border={border} placeholder={placeholder} rows={rows} />;
+function TextArea({
+  size = 'md',
+  dense = size === 'sm',
+  rows = 5,
+  ...props
+}: T.TextArea) {
+  return <S.TextArea $size={size} dense={dense} rows={rows} {...props} />;
 }
 
 export default TextArea;
