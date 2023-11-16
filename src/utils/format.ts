9c3b8d5b1ed61854af27f8162e27a98e8513e71b
nlom0218 <nlom0218@naver.com>
2023-11-16 12:54:05 +0900

    feat: format 유틸 생성

---
+++ src/utils/format.ts
@@ -0,0 +1,28 @@
+const format = {
+  date: (date: Date, delimiter?: '-' | '.') => {
+    if (!delimiter)
+      return `${date.getFullYear()}년 ${
+        date.getMonth() + 1
+      }월 ${date.getDate()}일`;
+
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    return `${date.getFullYear()}${delimiter}${
+      month < 10 ? `0${month}` : month
+    }${delimiter}${day < 10 ? `0${day}` : day}`;
+  },
+
+  time: (seconds: number) => {
+    const minutesFormatted = Math.floor(seconds / 60)
+      .toString()
+      .padStart(2, '0');
+    const secondsFormatted = Math.floor(seconds % 60)
+      .toString()
+      .padStart(2, '0');
+
+    return `${minutesFormatted}:${secondsFormatted}`;
+  },
+};
+
+export default format;
