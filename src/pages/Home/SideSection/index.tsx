415b07f4c9465bcb2030d95538ea064e6ea3ecd0
nlom0218 <nlom0218@naver.com>
2023-11-29 15:57:07 +0900

    refactor: Home SideSection 오늘의 급식 메뉴 로직 수정

--- src/pages/Home/SideSection/index.tsx
+++ src/pages/Home/SideSection/index.tsx
@@ -1,40 +1,25 @@
-import { useLunchMenuList } from '@Hooks/query/school/useLunchMenu';
+import { Suspense } from 'react';
+
 import useUserInfo from '@Hooks/useUserInfo';
-
-import dateFunctions from '@Utils/date';
-
-import SummaryList from '@Components/SummaryList';
 
 import * as S from './style';
 import { SideSectionProps } from './type';
 import HomeMemo from '../HomeMemo';
+import TodayLunchMenu from '../TodayLunchMenu';
 import VacationDate from '../VacationDate';
 
 function SideSection({ today }: SideSectionProps) {
-  const { isLoading, lunchMenuList } = useLunchMenuList({
-    date: dateFunctions.getToday(today),
-    type: 'day',
-  });
   const { userInfo } = useUserInfo();
 
-  const menus = lunchMenuList.length
-    ? lunchMenuList[0].menu?.map((item) => item.dish)
-    : [];
+  const school = userInfo?.school;
 
   return (
     <S.SideSection>
       <VacationDate season="여름" date={100} />
       <HomeMemo />
-      <SummaryList
-        title="오늘의 급식 메뉴"
-        list={menus}
-        isLoading={isLoading}
-        guideMessage={
-          !userInfo?.school
-            ? '학급운영 → 식단표에서 학교를 등록해보세요.'
-            : '오늘은 급식이 없어요.'
-        }
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        {school && <TodayLunchMenu today={today} />}
+      </Suspense>
     </S.SideSection>
   );
 }
