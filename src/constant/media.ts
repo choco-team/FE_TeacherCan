56d5c78013ec113853d78792fc1dbdff19fe9519
nlom0218 <nlom0218@naver.com>
2023-12-18 10:37:18 +0900

    feat: 반응형 MEDIA_QUERY 상수화

---
+++ src/constant/media.ts
@@ -0,0 +1,9 @@
+const MEDIA_SIZE = {
+  mobile: 768,
+  tablet: 1023,
+};
+
+export const MEDIA_QUERY = {
+  mobile: `@media(max-width: ${MEDIA_SIZE.mobile}px)`,
+  tablet: `@media(max-width: ${MEDIA_SIZE.tablet}px)`,
+};
