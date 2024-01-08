6ae85a575ba4fe07c2a0041d81293491fa8a36d9
nlom0218 <nlom0218@naver.com>
2024-01-08 16:13:13 +0900

    refactor: SideNavLink 조건부 렌더링 로직 개선

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -5,11 +5,7 @@ import { PiSunBold, PiMoonStarsBold } from 'react-icons/pi';
 import { Outlet, useLocation } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import useMedia from '@Hooks/useMedia';
-
 import route from '@Utils/route';
-
-import { ROUTE_PATH } from '@Constant/routePath';
 
 import ModalProvider from '@Providers/ModalProvider';
 import QueryProvider from '@Providers/QueryProvider';
@@ -25,7 +21,6 @@ import * as S from './style';
 
 function App() {
   const [isLightTheme, setIsLightTheme] = useState(true);
-  const media = useMedia();
 
   const { pathname } = useLocation();
   const [main] = route.getPathnames(pathname);
@@ -45,10 +40,7 @@ function App() {
                 <ModalProvider>
                   <Header pathname={pathname} />
                   <S.DefaultPageLayout>
-                    {media === 'desktop' &&
-                      pathname !== route.calculatePath([ROUTE_PATH.main]) && (
-                        <SideNavLink pathname={pathname} />
-                      )}
+                    <SideNavLink pathname={pathname} />
                     <S.PageWrapper>
                       <Outlet />
                     </S.PageWrapper>
